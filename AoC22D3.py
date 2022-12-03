5c7b33a4d09ec374beccec493853a48509099b90
iosifsalem <iosif.salem@gmail.com>
2022-12-03 18:18:13 +0100

    day 3 solution

---
+++ AoC22D3.py
@@ -0,0 +1,36 @@
+# advent of code 2022
+# day 3 (Rucksack Reorganization): https://adventofcode.com/2022/day/3
+
+import string
+
+with open("inputD3.txt", "r") as handle:
+    lines = handle.readlines()
+    
+all_letters = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+priority = lambda x : all_letters.index(x) + 1
+
+# part 1
+sum_of_priorities_part1 = 0
+
+for line in lines:
+    rucksack_middle = int(len(line[:-1])/2)
+    items_in_compartment1 = set(list(line[:rucksack_middle]))
+    items_in_compartment2 = set(list(line[rucksack_middle:-1]))
+    
+    intersection = items_in_compartment1.intersection(items_in_compartment2).pop()     
+    sum_of_priorities_part1 += priority(intersection)
+            
+print(f'Part 1: sum of priorities = {sum_of_priorities_part1}')
+
+# part 2
+sum_of_priorities_part2 = 0
+
+for i in range(int(len(lines)/3)):
+    rucksackA_items = set(list(lines[3*i][:-1]))
+    rucksackB_items = set(list(lines[3*i+1][:-1]))
+    rucksackC_items = set(list(lines[3*i+2][:-1]))
+    
+    badge = rucksackA_items.intersection(rucksackB_items.intersection(rucksackC_items)).pop()
+    sum_of_priorities_part2 += priority(badge)
+    
+print(f'Part 2: sum of priorities = {sum_of_priorities_part2}')
